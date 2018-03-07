58dee4139983b26e4e0104015ab444bf50edb637
kylin987 <kylin87@163.com>
2018-03-07 17:46:14 +0800

    加载控制器

--- core/imooc.php
+++ core/imooc.php
@@ -4,7 +4,20 @@ namespace core;
 
 class imooc {
 	static public function run(){
-		$route = new \core\lib\route();  
+		$route = new \core\lib\route();
+		
+		$ctrlClass = $route->ctrl;
+		$action = $route->action;
+		
+		$ctrlfile = APP.'/ctrl/'.$ctrlClass.'Ctrl.php';
+		$ctrlClass = '\\'.MODULE.'\ctrl\\'.$ctrlClass.'Ctrl';
+		if(is_file($ctrlfile)){
+			include $ctrlfile;
+			$ctrl = new $ctrlClass();
+			$ctrl->$action();
+		}else{
+			throw new \Exception('找不到控制器'.$ctrlClass);
+		}
 		
 	}
 	static public function load($class){
