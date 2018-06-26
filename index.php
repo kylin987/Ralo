41c74ff154da1b0e56903748ced77ae2d925bf28
kylin987 <kylin87@163.com>
2018-06-26 10:37:02 +0800

    增加composer

--- index.php
+++ index.php
@@ -10,11 +10,18 @@ define('IMOOC',realpath('./'));
 define('CORE',IMOOC.'/core');
 define('APP',IMOOC.'/app');
 define('MODULE','app');
-
 define('DEBUG',true);
 
+include "vendor/autoload.php";
+
 if(DEBUG){
-	ini_set('display_error',1);
+	$whoops = new \Whoops\Run; 
+	$title ="哎呀，框架出错了";
+	$option = new \Whoops\Handler\PrettyPageHandler();
+	$option->setPageTitle($title);
+    $whoops->pushHandler($option);  
+    $whoops->register();
+	ini_set('display_error','On');
 }else{
 	ini_set('display_error','Off');
 }
